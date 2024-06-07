6b8f0d012856149e29a6b15e4c5ce90fecef6d0a
Sadoon Al-Zubaidi <sal-zuba@3-B-3.42heilbronn.de>
2024-06-07 13:03:11 +0200

    First commit, Just created the needed Makefile and ceratin files and folders

---
+++ Makefile
@@ -0,0 +1,39 @@
+NAME = IRC
+NAME_TEST = ircserv_test
+CC = c++
+RM = rm -rf
+CFLAGS =  -std=c++98 -MD -Wall -Wextra -Werror
+
+SRC			=	Server main Users Channel Utils ServerCommands ServerEngine Bot
+SRC_TEST	=	Server test Users Channel Utils ServerCommands ServerEngine Bot
+
+SRC_DIR = src/
+OBJ_DIR = objects/
+
+SRCS		= $(addprefix $(SRC_DIR), $(addsuffix .cpp, $(SRC)))
+SRCS_TEST	= $(addprefix $(SRC_DIR), $(addsuffix .cpp, $(SRC_TEST)))
+
+OBJS		= $(addprefix $(OBJ_DIR), $(addsuffix .o, $(SRC)))
+OBJS_TEST	= $(addprefix $(OBJ_DIR), $(addsuffix .o, $(SRC_TEST)))
+
+all: $(NAME) 
+
+$(NAME): $(OBJS)
+	@$(CC) $(CFLAGS) -o $(NAME) $(OBJS)
+
+$(OBJ_DIR)%.o: $(SRC_DIR)%.cpp
+	@mkdir -p $(@D)
+	@$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	@$(RM) $(OBJ_DIR)
+
+fclean: clean
+	@$(RM) $(NAME) 
+
+test: $(OBJS_TEST)
+	@$(CC) $(CFLAGS) -o $(NAME_TEST) $(OBJS_TEST)
+	@clear
+
+re: fclean all
+.PHONY: all clean fclean re
---
+++ include/Lib.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Lib.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sal-zuba <sal-zuba@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/07 12:58:16 by sal-zuba          #+#    #+#             */
+/*   Updated: 2024/06/07 13:01:03 by sal-zuba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <string>
+#include <vector>
+#include <sstream>
+#include <cstdlib> 
+#include <cerrno> 
+#include <cstring>
+#include <iostream>
+#include "Users.hpp"
+
+
