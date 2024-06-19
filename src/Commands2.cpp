4216c2992882118f5a460bccdb4c3cc5081ba90d
akramguediri <akramguediri2@gmail.com>
2024-06-19 18:24:41 +0200

    added more details to the PRIVMSG command

--- src/Commands2.cpp
+++ src/Commands2.cpp
@@ -1,6 +1,26 @@
 #include "../include/Server.hpp"
 #include "../include/Users.hpp"
 #include "../include/Lib.hpp"
+#include <cstring>
+
+std::vector<std::string> split(const std::string &str, const char *delimiter) {
+    std::vector<std::string> tokens;
+    char *cstr = new char[str.length() + 1];
+    std::strcpy(cstr, str.c_str());
+
+    char *token = std::strtok(cstr, delimiter);
+    while (token != NULL) {
+        tokens.push_back(std::string(token));
+        token = std::strtok(NULL, delimiter);
+    }
+
+    delete[] cstr;
+    return tokens;
+}
+
+
+
+
 
 //-----------------Quit Command------------------//
 
@@ -45,24 +65,31 @@ bool isInChannel(const std::string& nickname, const std::vector<struct C_Users>&
     }
     return false;
 }
-void c_privmsg(Command cmd, Users user, std::string target, std::vector<Users *>	AllUsers, std::vector<Channel *> AllChannels){
-
+void c_privmsg(Command cmd, Users user, std::vector<Users *> AllUsers, std::vector<Channel *> AllChannels){
+    Channel *isChannel;
+    int isUser;
     std::string param = cmd.Rest;
-    Channel *isChannel = CheckChannel(AllChannels, target);
-    int isUser = CheckUser(AllUsers, target);
+    std::vector<std::string> paramList = split(param, " ");
+    std::vector<std::string> target = split(paramList[0], ",");
+    for (std::vector<std::string>::const_iterator it = target.begin(); it != target.end(); ++it){
+        isChannel = CheckChannel(AllChannels, *it);
+        isUser = CheckUser(AllUsers, *it);
 
-    if (isChannel)
-    {
-        for (std::vector<Users *>::const_iterator it = AllUsers.begin(); it != AllUsers.end(); ++it) {
-            if ((*it)->getSocket() != user.getSocket() && isInChannel((*it)->getNickname(), isChannel->UserList)) {
-                send((*it)->getSocket(), param.c_str(), param.length(), 0);
-        }
-    }
-    }
-    else if (isUser)
-        send(isUser, param.c_str(), param.length(), 0);
+        if (isChannel)
+            {
+                for (std::vector<Users *>::const_iterator it = AllUsers.begin(); it != AllUsers.end(); ++it) {
+                    if ((*it)->getSocket() != user.getSocket() && isInChannel((*it)->getNickname(), isChannel->UserList)) {
+                        send((*it)->getSocket(), param.c_str(), param.length(), 0);
+                    }
+                }
+            }
+        if (isUser)
+            send(isUser, param.c_str(), param.length(), 0);
     //else
         //error handeling with code number thingy 
+        isChannel = NULL;
+        isUser = NULL;
+    }
 }
 
 //---------------MODE Command------------------//
