e2199a915c1133c84f53842d704f0b7aa6c70299
Rmehadje <rayanmehadjeri6@gmail.com>
2024-06-26 11:42:44 +0200

    fixing bot

--- src/Bot.cpp
+++ src/Bot.cpp
@@ -30,31 +30,31 @@ std::string	Bot::getBotName() const{
 
 void	Bot::HelpCommands(Users *user){
 	std::string Help = "You ask and you shall receive, here is the list of commands:\n\n"
-	"- TOPIC:   <Channel> <TOPIC>,                      The TOPIC command serves the purpose of giving you the possibility of changing the Topic, or see the topic set for the channel\n"
-	"- KICK:    <Channel> <user>,                          The KICK command ejects a user from the Channel.\n"
-	"- INVITE:  <NickName> <Channel>,                The INVITE command hands you the power of inviting your peers to the Channel.\n"
-	"- PRIVMSG: <target>   <message>,                 It is quite self explanatory, this command is used to send a private message to someone.\n\n"
-	"- MODE: this is used to change Channel modes, for this you would need to be the OP, here are the modes available:\n"
-	"- +i: #Channel <+i>                                         Set or remove the invite-only for the channel.\n"
-	"- +t: #Channel <+t>                                        Set or remove the Topic restrictions for the OP.\n"
-	"- +k: #Channel <+K> <PassWord>                Set or remove Channel Password.\n"
-	"- +l: #Channel <+l> <int>                              Set or remove the limit of users in one Channel.\n"
-	"- +o: #Channel <+/-o> <NickName>            Set or remove the OP privilages to a user.\n\n"
-	"- As mentioned before, some of these commands require you to be the OP, you can't use KICK and MODE if you are a user, unless some restrtictions have been softened for you.";
+	"KNIGHT TOPIC:   <Channel> <TOPIC>,                      The TOPIC command serves the purpose of giving you the possibility of changing the Topic, or see the topic set for the channel\n"
+	"KNIGHT KICK:    <Channel> <user>,                          The KICK command ejects a user from the Channel.\n"
+	"KNIGHT INVITE:  <NickName> <Channel>,                The INVITE command hands you the power of inviting your peers to the Channel.\n"
+	"KNIGHT PRIVMSG: <target>   <message>,                 It is quite self explanatory, this command is used to send a private message to someone.\n\n"
+	"KNIGHT MODE: this is used to change Channel modes, for this you would need to be the OP, here are the modes available:\n"
+	"KNIGHT +i: #Channel <+i>                                         Set or remove the invite only for the channel.\n"
+	"KNIGHT +t: #Channel <+t>                                        Set or remove the Topic restrictions for the OP.\n"
+	"KNIGHT +k: #Channel <+K> <PassWord>                Set or remove Channel Password.\n"
+	"KNIGHT +l: #Channel <+l> <int>                              Set or remove the limit of users in one Channel.\n"
+	"KNIGHT +o: #Channel <+/-o> <NickName>            Set or remove the OP privilages to a user.\n\n"
+	"KNIGHT As mentioned before, some of these commands require you to be the OP, you can't use KICK and MODE if you are a user, unless some restrtictions have been softened for you.";
 	user->setBuffer(RPL_BOT_HC(Help));
 }
 
 void	Bot::BotCommands(Users *user){
-	std::string Help = "- If you would like to know what i can do, here is a list of my capabilities: \n"
-	"- User list: (write <BOT UL>) gives you all the users that are in the same server as you.\n"
-	"- Channel list: (write <BOT CL>), shows you all the available Channels created by you or other users.\n"
-	"- EightBall: (write <BOT Eightball>), you summon my capabilities of second sight by advising you wether you should, or should not act upon what you ask me.\n"
-	"- d20: (write <BOT d20>), permits you to test your luck out of 20.\n";
+	std::string Help = "KNIGHT If you would like to know what i can do, here is a list of my capabilities: \n"
+	"KNIGHT User list: (write <BOT UL>) gives you all the users that are in the same server as you.\n"
+	"KNIGHT Channel list: (write <BOT CL>), shows you all the available Channels created by you or other users.\n"
+	"KNIGHT EightBall: (write <BOT Eightball>), you summon my capabilities of second sight by advising you wether you should, or should not act upon what you ask me.\n"
+	"KNIGHT d20: (write <BOT d20>), permits you to test your luck out of 20.\n";
 	user->setBuffer(RPL_BOT_BC(user->getHostname(), Help));
 }
 
 void	Bot::UserList(Users *user, std::vector<Users *>	&AllUsers){
-	std::string Help = "- Here is the list for every user in the server:";
+	std::string Help = "KNIGHT Here is the list for every user in the server:";
 	for (std::vector <Users *>::iterator it = AllUsers.begin(); it != AllUsers.end(); it++){
 		Help += (*it)->getNickname();
 		Help += " / ";
@@ -65,10 +65,10 @@ void	Bot::UserList(Users *user, std::vector<Users *>	&AllUsers){
 void	Bot::ChannelList(Users *user, std::vector<Channel *> &AllChannels){
 	std::string everychannel;
 	if (AllChannels.empty()){
-		everychannel = "- There are no Channels available currently, feel free to create one!";
+		everychannel = "KNIGHT There are no Channels available currently, feel free to create one!";
 	}
 	else{
-		everychannel = "- Here is the list for every every channel in the server: ";
+		everychannel = "KNIGHT Here is the list for every every channel in the server: ";
 		for (std::vector<Channel *>::iterator it = AllChannels.begin(); it != AllChannels.end(); it++){
 			everychannel += (*it)->getName();
 			everychannel += " / ";
@@ -79,28 +79,28 @@ void	Bot::ChannelList(Users *user, std::vector<Channel *> &AllChannels){
 
 void	Bot::EightBall(Users *user){
     static const char* responses[] = {
-   	"- It appears so.",
-   	"- You'll learn from this.",
-   	"- It may not go as planned.",
-   	"- Think further about it.",
-   	"- I wouldn't know.",
-   	"- I shouldn't say.",
-   	"- It is favorable.",
-   	"- Perhaps another time.",
-   	"- Guaranteed.",
-   	"- The time is right.",
-   	"- Don't let that distract you.",
-   	"- You have other things to worry about.",
-   	"- Very well.",
-   	"- There is a chance.",
-   	"- It could be done.",
-   	"- I would not do that.",
-   	"- Doubtful.",
-   	"- No doubt in me.",
-   	"- Maybe in another timeline.",
-   	"- It would mean farewell.",
-   	"- It would bring novelty.",
-   	"- Focus, and ask again when you know more."};
+   	"KNIGHT It appears so.",
+   	"KNIGHT You'll learn from this.",
+   	"KNIGHT It may not go as planned.",
+   	"KNIGHT Think further about it.",
+   	"KNIGHT I wouldn't know.",
+   	"KNIGHT I shouldn't say.",
+   	"KNIGHT It is favorable.",
+   	"KNIGHT Perhaps another time.",
+   	"KNIGHT Guaranteed.",
+   	"KNIGHT The time is right.",
+   	"KNIGHT Don't let that distract you.",
+   	"KNIGHT You have other things to worry about.",
+   	"KNIGHT Very well.",
+   	"KNIGHT There is a chance.",
+   	"KNIGHT It could be done.",
+   	"KNIGHT I would not do that.",
+   	"KNIGHT Doubtful.",
+   	"KNIGHT No doubt in me.",
+   	"KNIGHT Maybe in another timeline.",
+   	"KNIGHT It would mean farewell.",
+   	"KNIGHT It would bring novelty.",
+   	"KNIGHT Focus, and ask again when you know more."};
 
     const int num_responses = sizeof(responses) / sizeof(responses[0]);
     std::random_device rd;
@@ -114,7 +114,7 @@ void	Bot::EightBall(Users *user){
 void	Bot::d20(Users *user){
 	std::random_device rd;
    std::mt19937 gen(rd());
-	std::string str = "- You have summoned the d20...";
+	std::string str = "KNIGHT You have summoned the d20...";
    std::uniform_int_distribution<> dis(1, 20);
 	int dice = dis(gen);
 	str += std::to_string(dice);
