926ffec4d9a3fba1de67143533743743ee05ba1c
akramguediri <akramguediri2@gmail.com>
2024-06-27 14:10:12 +0200

    part function added, plus the reply message.

--- src/Part.cpp
+++ src/Part.cpp
@@ -9,18 +9,22 @@ std::vector<std::string> split(const std::string &str, const char *delimiter);
 void Server::c_part(Command cmd, Users* user)
 {
     if (cmd.params[0].empty())
-        return(user->setBuffer(ERR_NEEDMOREPARAMS(user->getHostname(), cmd.CmdName)));
+        return(user->setBuffer(ERR_NEEDMOREPARAMS(this->getHost(), cmd.CmdName)));
     std::string reason = cmd.params[1];
     std::vector<std::string> channels = split(cmd.params[0], ",");
     for (std::vector<std::string>::const_iterator it = channels.begin(); it != channels.end(); ++it) {
         Channel *channel = getChannel(*it);
-        if (channel == NULL)
-            return(user->setBuffer(ERR_NOSUCHCHANNEL(user->getHostname(), *it)));
-        if (!channel->UserIsInC(user))
-            return(user->setBuffer(ERR_NOTONCHANNEL(user->getHostname(), user->getNickname())));
+        if (channel == NULL){
+            user->setBuffer(ERR_NOSUCHCHANNEL(this->getHost(), *it));
+            continue ;
+        }
+        if (!channel->UserIsInC(user)){
+            user->setBuffer(ERR_NOTONCHANNEL(this->getHost(), user->getNickname()));
+            continue ;
+        }
         channel->deleteUserfromC(user);
+    user->setBuffer(RPL_PART(user->getSrc(), *it, ));
 
     }
-    // return(user->setBuffer(RPL_(user->getHostname(), user->getNickname())));
 
 }
