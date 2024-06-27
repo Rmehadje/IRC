15923e8aad8512c128d1de79d3df56dea09c1631
Sal-Zuba <alzubaidisadooon@gmail.com>
2024-06-27 12:42:28 +0200

    Added comments

--- include/Replies.hpp
+++ include/Replies.hpp
@@ -1,14 +1,14 @@
 #pragma once
 
 #define ERR_BADCHANMASK(chan)									": 476 " + chan + " :Bad Channel Mask\r\n"	   
-#define ERR_NOSUCHNICK(src, nick, chan)					":" + src + " 401 " + nick + " " + chan + " :No such Nick/channel\r\n"
+#define ERR_NOSUCHNICK(src, nick, chan)					":" + src + " 401 " + nick + " " + chan + " :No such Nick/channel\r\n" //
 #define ERR_NOSUCHSERVER(src, serv)							":" + src + " 402 " + serv + " :No such server\r\n"
 #define ERR_NOSUCHCHANNEL(src, chan)						":" + src + " 403 " + chan + " :No such channel\r\n"
 #define ERR_CANNOTSENDTOCHAN(src, chan)					":" + src + " 404 " + chan + " :Cannot send to channel\r\n"
 #define ERR_TOOMANYCHANNELS(src, chan)						":" + src + " 405 " + chan + " :You have joined too many channels\r\n"
 #define ERR_NOTEXTTOSEND(src, nick)							":" + src + " 412 " + nick + " :No text to send\r\n"
 #define ERR_INPUTTOOLONG(src)									":" + src + " 417 :Input line too long\r\n"
-#define ERR_UNKNOWNCOMMAND(src, cmd, nick)				":" + src + " 421 " + nick + " " + cmd + " :Unknown command\r\n"
+#define ERR_UNKNOWNCOMMAND(src, cmd, nick)				":" + src + " 421 " + nick + " " + cmd + " :Unknown command\r\n" 
 #define ERR_NONICKNAMEGIVEN(src)								":" + src + " 431 :No nickname given\r\n"
 #define ERR_ERRONEUSNICKNAME(src, nick)					":" + src + " 432 " + nick + " :Erroneus nickname\r\n"
 #define ERR_NICKNAMEINUSE(src, nick)						":" + src + " 433 " + nick + " :Nickname is already in use\r\n"
@@ -29,28 +29,28 @@
 #define ERR_USERDONTMATCH(src)								":" + src + " 502 :Cant change mode for other users\r\n"
 
 //replies
-#define RPL_NAMERPLY(host, nick, chan, list)				":" + host + " 353 " + nick + " = " + chan + " :" + list + "\r\n"
-#define RPL_ENDOFNAMES(host, nick, chan)              ":" + host + " 366 " + nick + " " + chan + " :End of /NAMES list.\r\n"
-#define RPL_PRIVMSG(src, dis_nick, msg)					":" + src + " PRIVMSG " + dis_nick + " :" + msg + "\r\n"
-#define RPL_NICK(src, nick)									":" + src + " :Your nickname has been set to " + nick + "\r\n"
-#define RPL_PASS(src)											":" + src + " :Password is correct, you may continue the registration\r\n"
+#define RPL_NAMERPLY(host, nick, chan, list)				":" + host + " 353 " + nick + " = " + chan + " :" + list + "\r\n" //
+#define RPL_ENDOFNAMES(host, nick, chan)              ":" + host + " 366 " + nick + " " + chan + " :End of /NAMES list.\r\n" //
+#define RPL_PRIVMSG(src, dis_nick, msg)					":" + src + " PRIVMSG " + dis_nick + " :" + msg + "\r\n" //
+#define RPL_NICK(src, nick)									":" + src + " :Your nickname has been set to " + nick + "\r\n" //
+#define RPL_PASS(src)											":" + src + " :Password is correct, you may continue the registration\r\n" //
 #define RPL_USER(src, user)									":" + src + " :Your username is now set to: " + user + "\r\n"
 #define RPL_NOTOPERATORANYMORE(src, chan)					":" + src + " :You are no longer an IRC operator in " + chan + "\r\n"
 #define RPL_BECOMINGOPERATOR(src, chan)					":" + src + " :You are now an IRC operator in " + chan + "\r\n"
-#define RPL_WHOISUSER(src, nick, user, username, host, rname)			":" + src + " 311 " + nick + " " + user + " " + username + " " + host + " * :" + rname + "\r\n"
-#define RPL_ENDOFWHOIS(src, user, name)										":" + src + " 318 " + user + " " + name + " :End of WHOIS list\r\n"
+#define RPL_WHOISUSER(src, nick, user, username, host, rname)			":" + src + " 311 " + nick + " " + user + " " + username + " " + host + " * :" + rname + "\r\n"//
+#define RPL_ENDOFWHOIS(src, user, name)										":" + src + " 318 " + user + " " + name + " :End of WHOIS list\r\n" //
 #define RPL_CAP(src)												":" + src + " CAP * LS : you may enter PASS now...\r\n"
 // #define RPL_NICKCHANGE(src, nick)							":" + src + " Your new nickname has been set to " + nick + "\r\n"
-#define RPL_QUIT(src, nick, reply)							":" + src + " " + nick + " " + reply + "\r\n"
-#define RPL_KICK(src, chan, reason, user)					":" + src + " KICK " + chan + " " + user + " :" + reason + "\r\n"
+#define RPL_QUIT(src, nick, reply)							":" + src + " " + nick + " " + reply + "\r\n" 
+#define RPL_KICK(src, chan, reason, user)					":" + src + " KICK " + chan + " " + user + " :" + reason + "\r\n" //
 #define RPL_PART(src, chan, nick)							":" + src + " " + nick + " has parted from " + chan + "\r\n"
 #define RPL_WELCOME(src, user, host, nick)				":" + src + " 001 " + nick + " :Welcome to the IRC_server network " + nick + "!" + user + "@" + host + "\r\n"
 #define RPL_UMODEIS(src, nick, mode)						":" + src + " 221 " + nick + " " + mode + "\r\n"
 #define RPL_CHANNELMODEIS(src, chan, modest, modea)	":" + src + " 324 " + chan + " " + modest + " " + modea + "\r\n"
 #define RPL_NOTOPIC(src,nick, chan)								":" + src + " 331 " + nick + " " + chan + " :No topic is set\r\n"
 #define RPL_TOPIC(host, nick, chan, topic)				":" + host + " 332 " + nick + " " + chan + " :" + topic + "\r\n"
-#define RPL_NICKCHANGE(oldnick, user, host, newnick)  ":" + oldnick + "!" + user + "@" + host + " NICK :" + newnick + "\r\n"
-#define RPL_JOIN(nick, user, user_host, chan)							":" + nick + "!" + user + "@" + user_host + " JOIN :" + chan + "\r\n"
+#define RPL_NICKCHANGE(oldnick, user, host, newnick)  ":" + oldnick + "!" + user + "@" + host + " NICK :" + newnick + "\r\n" //
+#define RPL_JOIN(nick, user, user_host, chan)							":" + nick + "!" + user + "@" + user_host + " JOIN :" + chan + "\r\n" //
 
 
 //Bot replies
